00ec3c9829b98d1cf516e46b2819ee89ddebe780
Mudassir <mughalmudassir966@gmail.com>
2025-09-02 14:58:40 +0500

    feat: added migrations & sqlc

---
+++ db/queries/users.sql
@@ -0,0 +1,7 @@
+-- name: CreateUser :one
+INSERT INTO users (
+  id, name, username, password, created_at, updated_at
+) VALUES (
+  ?, ?, ?, ?, ?, ?
+)
+RETURNING id, name, username, created_at, updated_at;
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	"embed"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pressly/goose/v3"
+)
+
+var (
+	DB_CONN *sql.DB
+	//go:embed db/schema/*.sql
+	embedMigrations embed.FS
 )
 
 const (
@@ -21,9 +31,35 @@ func init() {
 	validateEnv(port, "PORT")
 
 	log.Println("env variables loaded")
+
+	log.Println("making a connection with db")
+
+	conn, err := sql.Open("sqlite3", DB_PATH)
+	if err != nil {
+		log.Fatalf("failed to form a connection: %v", err)
+	}
+	DB_CONN = conn
+
+	log.Println("connection DB formed!")
 }
 
-func main() {}
+// running migrations
+func init() {
+	log.Println("running migrations")
+
+	goose.SetDialect("sqlite3")
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.Up(DB_CONN, "db/schema"); err != nil { //
+		log.Fatalf("failed to run migrations: %v", err)
+	}
+
+	log.Println("migrations ran successfully!")
+}
+
+func main() {
+	defer DB_CONN.Close()
+}
 
 func validateEnv(env, envName string) {
 	if env == "" {
